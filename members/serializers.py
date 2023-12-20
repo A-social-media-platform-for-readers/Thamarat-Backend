3300cf858f4e480185df02c41bf5baa4833cd4c1
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-12-20 21:15:52 +0200

    done

--- members/serializers.py
+++ members/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Gender, Address, Member, Reader, Author, PublishingHouse
 
 
+
 class GenderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Gender
