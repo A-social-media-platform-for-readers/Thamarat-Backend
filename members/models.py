a5dfe612e136dd3d77f45a87b3748327f8ce532a
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2024-02-10 22:29:50 +0200

    member model update

--- members/models.py
+++ members/models.py
@@ -18,25 +18,8 @@ class Address(models.Model):
     country = models.CharField(max_length=255)
 
 
-class Reader(models.Model):
-    Reader_id = models.BigAutoField(primary_key=True)
-    reader_birth_date = models.DateField()
-    reader_gender = models.CharField(max_length=55, choices=Gender, default="M")
-
-
-class Author(models.Model):
-    author_id = models.BigAutoField(primary_key=True)
-    author_birth_date = models.DateField()
-    author_gender = models.CharField(max_length=55, choices=Gender, default="M")
-
-
-class PublishingHouse(models.Model):
-    publishing_house_id = models.BigAutoField(primary_key=True)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20)
-
-
-class Member(Reader, Author, PublishingHouse):
+class Member(models.Model):
+    # common fields
     Member_id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -44,11 +27,16 @@ class Member(Reader, Author, PublishingHouse):
     password = models.CharField(max_length=255)
     status = models.CharField(max_length=55, choices=AccountStatus)
     dateOfMembership = models.DateTimeField(auto_now_add=True)
-    identity = models.CharField(
-        choices=(
-            ("reader", "Reader"),
-            ("author", "Author"),
-            ("publishingHouse", "PublishingHouse"),
-        ),
-        max_length=255,
+    identity = (
+        ("reader", "Reader"),
+        ("author", "Author"),
+        ("publisher", "Publishing House"),
     )
+
+    # specific fields for reader and author
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=55, choices=Gender, default="M")
+
+    # specific fields for publisher
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
