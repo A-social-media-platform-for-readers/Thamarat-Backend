2e456ded233f7ff740df5b10b336c865979e9835
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2024-02-10 22:05:35 +0200

    add api

--- members/urls.py
+++ members/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("members/", views.Member_list),
-    path("members/<int:pk>/", views.Member_detail),
+    path('api/all',views.all_Member,),
+    path('api/add',views.add_Member,),
+    path('api/delete/<int:ID>',views.delete_Member,),
+    path('api/update/<int:ID>',views.update_Member,),
 ]
--- members/views.py
+++ members/views.py
@@ -8,41 +8,82 @@ from .models import Member
 from .serializers import *
 
 
-@api_view(["GET", "POST"])
-def Member_list(request):
-    if request.method == "GET":
-        Members = Member.objects.all()
-        serializer = MemberSerializer(Members, many=True)
-        return Response(serializer.data)
+# @api_view(["GET", "POST"])
+# def Member_list(request):
+#     if request.method == "GET":
+#         Members = Member.objects.all()
+#         serializer = MemberSerializer(Members, many=True)
+#         return Response(serializer.data)
 
-    elif request.method == "POST":
-        # data = JSONParser().parse(request)
-        serializer = MemberSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == "POST":
+#         # data = JSONParser().parse(request)
+#         serializer = MemberSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def Member_detail(request, pk):
-    try:
-        member = Member.objects.get(pk=pk)
-    except Member.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# @api_view(["GET", "PUT", "DELETE"])
+# def Member_detail(request, pk):
+#     try:
+#         member = Member.objects.get(pk=pk)
+#     except Member.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == "GET":
-        serializer = MemberSerializer(Member)
-        return Response(serializer.data)
+#     if request.method == "GET":
+#         serializer = MemberSerializer(Member)
+#         return Response(serializer.data)
 
-    elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = MemberSerializer(Member, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == "PUT":
+#         data = JSONParser().parse(request)
+#         serializer = MemberSerializer(Member, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
-        Member.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == "DELETE":
+#         Member.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def all_Member(req):
+    
+    objs = Member.objects.all()
+    jsndata = MemberSerializer(objs, many=True).data
+    finaldata = {'data': jsndata}
+
+    return Response(data=finaldata, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def add_Member(req):
+    
+    Member = MemberSerializer(data=req.data)
+    if (Member.is_valid()):
+
+        Member.save()
+
+    return Response(status.HTTP_201_CREATED)
+
+
+@api_view(['DELETE'])
+def delete_Member(req, ID):
+    
+    Member.objects.filter(id=ID).delete()
+
+    return Response(status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['PUT'])
+def update_Member(req, ID):
+    
+    t = Member.objects.filter(id=ID)[0]
+    st = MemberSerializer(instance=t, data=req.data, partial=True)
+    
+    if (st.is_valid()):
+        
+        st.save()
+
+    return Response(status.HTTP_200_OK)
