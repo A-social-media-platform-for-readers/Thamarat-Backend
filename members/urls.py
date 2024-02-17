812345b0ccbeba25ebab6d0a035e2cd2f87c8162
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2024-02-17 20:14:44 +0200

    url

--- members/urls.py
+++ members/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/all',views.all_Member,),
-    path('api/add',views.add_Member,),
-    path('api/delete/<int:ID>',views.delete_Member,),
-    path('api/update/<int:ID>',views.update_Member,),
+    path('member/api/all',views.all_Member,),
+    path('member/api/add',views.add_Member,),
+    path('member/api/delete/<int:ID>',views.delete_Member,),
+    path('member/api/update/<int:ID>',views.update_Member,),
 ]
